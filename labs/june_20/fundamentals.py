0a39c0867a0e7e67cf1a195f05f9b83b4300ded4
YourFavoritePoser <youfavoriteposer@gmail.com>
2024-06-26 21:39:59 -0700

    added learning labs 6/20 & 6/24

---
+++ labs/june_20/fundamentals.py
@@ -0,0 +1,14 @@
+print("hello there.") #hello there.
+
+name = "LeBron" #str
+age = 35 #int
+salary = 37.44 #float
+plays_basketball = True #bool
+jersey_number = "23" #str
+
+car_name = "Toyota"
+x = 50
+first_name = "Robert"
+
+print(name, age, salary, plays_basketball, jersey_number)
+type(name, age, salary, plays_basketball, jersey_number)
