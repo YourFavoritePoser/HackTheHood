76f673de947456b83a21328d866e55d0b3cf29c7
YourFavoritePoser <youfavoriteposer@gmail.com>
2024-07-08 22:44:50 -0700

    updated attack to randomize damage for every attack

--- labs/hero/weapon.py
+++ labs/hero/weapon.py
@@ -4,7 +4,6 @@ class Weapon:
     def __init__(self, name, max_damage):
         self.name = name
         self.max_damage = max_damage
-        self.damage = random.randint(0, max_damage)
 
     def attack(self):
-        return self.damage
+        return self.max_damage
