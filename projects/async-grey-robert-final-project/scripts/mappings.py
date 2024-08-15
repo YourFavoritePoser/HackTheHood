4132ede8a9cb8ff764577f2b248a7061c1c5d3a8
YourFavoritePoser <youfavoriteposer@gmail.com>
2024-08-14 19:08:45 -0700

    add beat_to_neighborhood dict

--- projects/async-grey-robert-final-project/scripts/mappings.py
+++ projects/async-grey-robert-final-project/scripts/mappings.py
@@ -3,3 +3,57 @@ beat_to_area = {'01X': 'Area 1', '02X': 'Area 1', '02Y': 'Area 1', '03X': 'Area 
                 '15X': 'Area 3', '16X': 'Area 3', '16Y': 'Area 3', '17X': 'Area 3', '17Y': 'Area 3', '18X': 'Area 3', '18Y': 'Area 3', '19X': 'Area 3', '20X': 'Area 3', '21X': 'Area 3', '21Y': 'Area 3', '22X': 'Area 3', '22Y': 'Area 3',
                 '23X': 'Area 4', '24X': 'Area 4', '24Y': 'Area 4', '25X': 'Area 4', '25Y': 'Area 4', '26X': 'Area 4', '26Y': 'Area 4', '27X': 'Area 4', '27Y': 'Area 4', '28X': 'Area 4',
                 '29X': 'Area 5', '30X': 'Area 5', '30Y': 'Area 5', '31X': 'Area 5', '31Y': 'Area 5', '31Z': 'Area 5', '32X': 'Area 5', '32Y': 'Area 5', '33X': 'Area 5', '34X': 'Area 5', '35X': 'Area 5', '35Y': 'Area 5'}
+
+beat_to_neighborhood = {
+    # Area 1: West Oakland and Downtown
+    '01X': 'West Oakland',
+    '02X': 'West Oakland',
+    '02Y': 'West Oakland',
+    '03X': 'West Oakland',
+    '05X': 'Downtown Oakland / Jack London Square',
+    '06X': 'Downtown Oakland / Uptown',
+    '07X': 'West Oakland (Prescott)',
+    '09X': 'Northgate / KONO',
+    '10X': 'Lake Merritt / Adams Point',
+    '10Y': 'Lake Merritt / Adams Point',
+    '11X': 'Piedmont Avenue / Grand Lake',
+    
+    # Area 2: North Oakland
+    '08X': 'North Oakland (Temescal)',
+    '12X': 'Rockridge / Temescal',
+    '13X': 'Rockridge / Claremont',
+    '13Y': 'Upper Rockridge / Claremont',
+    '14X': 'Montclair',
+    '15X': 'Piedmont',
+    '16X': 'Upper Rockridge / Shepherd Canyon',
+    
+    # Area 3: East Oakland (North) and Central Oakland
+    '16Y': 'Laurel',
+    '17X': 'Allendale / Fairfax',
+    '17Y': 'Redwood Heights',
+    '18X': 'Brookfield Village',
+    '18Y': 'Eastmont',
+    '19X': 'Sobrante Park',
+    '19Y': 'Sobrante Park',
+    '21X': 'Fruitvale',
+    '21Y': 'Fruitvale',
+    '22X': 'San Antonio / Highland Park',
+    
+    # Area 4: East Oakland (South)
+    '22Y': 'Maxwell Park',
+    '23X': 'Havenscourt / Lockwood Gardens',
+    '24X': 'Coliseum',
+    '25X': 'Elmhurst',
+    '26X': 'Millsmont',
+    '27X': 'Mills College / Seminary',
+    '28X': 'Seminary / Fairfax',
+    
+    # Area 5: Far East Oakland
+    '29X': 'Melrose',
+    '30X': 'Havenscourt',
+    '31X': 'Seminary',
+    '32X': 'Eastmont / Coliseum Industrial',
+    '33X': 'Elmhurst',
+    '34X': 'Coliseum / Coliseum Industrial',
+    '35X': 'Brookfield Village / Elmhurst',
+}
