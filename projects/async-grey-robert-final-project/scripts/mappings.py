decbe3a05a30fd807551d0ea7cbb14e8db803d19
YourFavoritePoser <youfavoriteposer@gmail.com>
2024-08-16 15:15:21 -0700

    clear generate_map.py to try again

--- projects/async-grey-robert-final-project/scripts/mappings.py
+++ projects/async-grey-robert-final-project/scripts/mappings.py
@@ -2,14 +2,12 @@ beat_to_area = {'01X': 'Area 1', '02X': 'Area 1', '02Y': 'Area 1', '03X': 'Area 
                 '08X': 'Area 2', '09X': 'Area 2', '10X': 'Area 2', '10Y': 'Area 2', '11X': 'Area 2', '12X': 'Area 2', '12Y': 'Area 2', '13X': 'Area 2', '13Y': 'Area 2', '13Z': 'Area 2', '14X': 'Area 2', '14Y': 'Area 2',
                 '15X': 'Area 3', '16X': 'Area 3', '16Y': 'Area 3', '17X': 'Area 3', '17Y': 'Area 3', '18X': 'Area 3', '18Y': 'Area 3', '19X': 'Area 3', '20X': 'Area 3', '21X': 'Area 3', '21Y': 'Area 3', '22X': 'Area 3', '22Y': 'Area 3',
                 '23X': 'Area 4', '24X': 'Area 4', '24Y': 'Area 4', '25X': 'Area 4', '25Y': 'Area 4', '26X': 'Area 4', '26Y': 'Area 4', '27X': 'Area 4', '27Y': 'Area 4', '28X': 'Area 4',
-                '29X': 'Area 5', '30X': 'Area 5', '30Y': 'Area 5', '31X': 'Area 5', '31Y': 'Area 5', '31Z': 'Area 5', '32X': 'Area 5', '32Y': 'Area 5', '33X': 'Area 5', '34X': 'Area 5', '35X': 'Area 5', '35Y': 'Area 5'}
+                '29X': 'Area 5', '30X': 'Area 5', '30Y': 'Area 5', '31X': 'Area 5', '31Y': 'Area 5', '31Z': 'Area 5', '32X': 'Area 5', '32Y': 'Area 5', '33X': 'Area 5', '34X': 'Area 5', '35X': 'Area 5', '35Y': 'Area 5'
+                }
 
-# Dictionary mapping police beats to neighborhood names
-
-area_to_neighborhood = {
-    'Area 1': 'Downtown & West Oakland (Area 1)',
-    'Area 2': 'North Oakland (Area 2)',
-    'Area 3': 'Lake Merritt East Oakland (Area 3)',
-    'Area 4': 'High St. East Oakland (Area 4)',
-    'Area 5': 'Deep East Oakland & Airport (Area 5)',
+area_to_neighborhood = {'Area 1': 'Downtown & West Oakland (Area 1)',
+                        'Area 2': 'North Oakland (Area 2)',
+                        'Area 3': 'Lake Merritt East Oakland (Area 3)',
+                        'Area 4': 'High St. East Oakland (Area 4)',
+                        'Area 5': 'Deep East Oakland & Airport (Area 5)',
 }
